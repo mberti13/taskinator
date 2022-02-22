7d11bb2fcfd649cf2575b9f6aff1c28ceba75289
MJB <matt.berti13@gmail.com>
2022-02-22 15:30:51 -0600

    added input values to form to save to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,23 @@ var createTaskHandler = function(){
     //Prevent browser from refreshing on form submission
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name= 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
+
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     taskToDoEl.appendChild(listItemEl);
 }
-//Event Listener for "click"
+//Event Listener for "click" or enter/ SUBMIT ON FORM!
 formEl.addEventListener("submit", createTaskHandler);
